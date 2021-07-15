2d3aa6aaf24d23ed1be71f27291f1b890c88d555
Nisha Chandramoorthy <nishac92@gmail.com>
2021-07-15 16:54:28 -0400

    Add test for tent map

--- tests/test_tent.jl
+++ tests/test_tent.jl
@@ -7,7 +7,16 @@ function test_attractor()
     fig, ax = subplots()
     ax.plot(x, fx, ".")
 end
-function test_gaussian()
+function test_gaussian_pushforward(n)
+	x = randn(1000000)
+	s = 0.2
+	for i = 1:n
+		x .= next.(x,s)
+	end
+	hist(x,density=true,bins=100)
+	xlabel(L"x",fontsize=30)
+	ylabel("\$ \\tilde{\\varphi}^$(n)_\\sharp \\eta \$",fontsize=30)
+	tick_params(axis="both",labelsize=30)
 
 end
 
