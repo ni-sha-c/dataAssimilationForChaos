5c0471b83aa33ab9c5abf257c0af2e0b5cc3a5c8
Nisha Chandramoorthy <nishac@mit.edu>
2021-07-16 09:19:22 -0400

    finish writing sir

--- src/sir.jl
+++ src/sir.jl
@@ -1,25 +1,36 @@
-for k = 1:K
-    for i = 1: N_p 
-	    w[i] = w[i]*
-    end
-    sumw = sum(w)
-	w ./= sum(w)
-	N_eff = 1.0/sum(w.*w)
-    if (N_eff < N_thr)
+σ_o = 1.0
+function p_y_g_x(a)
+    return 1/sqrt(2π)/σ_o*exp(-0.5*a*a/σ_o/σ_o)
+end
+function obs(x)
+    return x
+end
+function sir(y)
+	K = size(y)[1]
+	for k = 1:K
+    	for i = 1: N_p 
+		    w[i] = w[i]*p_y_g_x(y[k] - obs(x[i]))
+    	end
+        sumw = sum(w)
+	    w ./= sum(w)
+	    N_eff = 1.0/sum(w.*w)
+        if (N_eff < N_thr)
 	    #resample
-        for j = 1:N_p
-			r = rand()
-			cdfw = cumsum(w)
-			new_pts[j] = 1
-			for i = 1:N_p
-		    	if cdfw[i] >= r
-				    new_pts[j] = i
-			        break
-			    end
+            for j = 1:N_p
+			    r = rand()
+			    cdfw = cumsum(w)
+			    new_pts[j] = 1
+			    for i = 1:N_p
+		    	    if cdfw[i] >= r
+				        new_pts[j] = i
+			            break
+			        end
+		        end
+
 		    end
-
-		end
-
+	    end
+    end
+end
 						
 
 
