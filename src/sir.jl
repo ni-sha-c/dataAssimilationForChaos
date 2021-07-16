454612582c826ebbf6a3c3e13596a02db7f1580d
Nisha Chandramoorthy <nishac@mit.edu>
2021-07-16 09:54:57 -0400

    finish logic for resampling

--- src/sir.jl
+++ src/sir.jl
@@ -5,29 +5,31 @@ end
 function obs(x)
     return x
 end
-function sir(y)
+function sir(y,x,N_thr=10)
 	K = size(y)[1]
+	N_p = size(x)[1]
+    w = ones(N_p)./N_p
+	new_pts = similar(x)
 	for k = 1:K
     	for i = 1: N_p 
 		    w[i] = w[i]*p_y_g_x(y[k] - obs(x[i]))
     	end
-        sumw = sum(w)
-	    w ./= sum(w)
+		w ./= sum(w)
 	    N_eff = 1.0/sum(w.*w)
         if (N_eff < N_thr)
-	    #resample
+	        #resample
+            cdfw = cumsum(w)
+			new_pts .= similar(x)
             for j = 1:N_p
 			    r = rand()
-			    cdfw = cumsum(w)
-			    new_pts[j] = 1
 			    for i = 1:N_p
 		    	    if cdfw[i] >= r
-				        new_pts[j] = i
+					    new_pts[j] = x[i]
 			            break
 			        end
 		        end
-
 		    end
+			x .= new_pts
 	    end
     end
 end
