20ddd6b1718e226b0ad3bb734d3552a1345988d2
Nisha Chandramoorthy <nishac@mit.edu>
2021-07-20 12:52:25 -0400

    at least no nans are coming out

--- examples/lorenz.jl
+++ examples/lorenz.jl
@@ -1,13 +1,17 @@
 dt = 0.002
-function flow(u::Array{Float64,1},s::Array{Float64,1})
+s = [10.0, 28.0, 8/3]
+d = 3
+function flow(u::Array{Float64,1})
     sigma, rho, beta = s
     x, y, z = u[1], u[2], u[3]
     return [sigma*(y - x), x*(rho - z) - y, x*y - beta*z]
 end
-function next(u, s)
-    k1 = dt*flow(u, s)
-    k2 = dt*flow(u .+ k1, s)
+function next(u, sn)
+    k1 = dt*flow(u)
+    k2 = dt*flow(u .+ k1)
     u_next = @. u + (k1 + k2)/2 
+	noise = sn*randn(d)
+	u_next .+= noise
     return u_next
 end
 
--- src/sir.jl
+++ src/sir.jl
@@ -1,33 +1,35 @@
 include("../examples/lorenz.jl")
 using JLD
-σ_o = 0.1
+σ_o = 1.0
 function p_y_g_x(a)
-		return log(1/sqrt(2π)/σ_o) -0.5*a*a/σ_o/σ_o
+		term =  log(1/sqrt(2π)/σ_o) 
+		return sum(term .- 0.5*a.*a/σ_o/σ_o)
 end
 function obs(x)
-	return x[3]
+	return x
 end
 function sir(y,x_ip,s,N_thr=10)
-	K = size(y)[1]
-	N_p = size(x)[2]
-	d = size(x)[1]
-	weights = zeros(N_p, K)
+	K = size(y)[2]
+	N_p = size(x_ip)[2]
+	w_trj = zeros(N_p, K)
 	x_trj = zeros(d, N_p, K)
 
 
     w = ones(N_p)./N_p
 	new_pts = similar(x_ip)
 	x = copy(x_ip)
-	
 	for k = 1:K
     	for i = 1: N_p 
-				logwi = log(w[i]) + p_y_g_x(y[k] - obs(x[:,i]))
+				logwi = log(w[i]) + p_y_g_x(y[:,k] .- obs(x[:,i]))
 				w[i] = exp(logwi)
     	end
-		@show sum(w)
+
 		w ./= sum(w)
-	    N_eff = 1.0/sum(w.*w)
-        if (N_eff < N_thr)
+
+		N_eff = 1.0/sum(w.*w)
+        
+		@show minimum(w), maximum(w), N_eff
+		if (N_eff < N_thr)
 	        #resample
             # multinomial resampling
 			cdfw = cumsum(w)
@@ -42,27 +44,40 @@ function sir(y,x_ip,s,N_thr=10)
 		        end
 		    end
 			x .= new_pts
+			@show x
 	    end
-		x .= next.(x,s)
-		part_pos[:,:,k] .= x
-		weights[:,k] .= w
+		for i = 1:N_p
+		    x[:,i] .= next(x[:,i],s)
+		end
+
+		@show x
+		x_trj[:,:,k] .= x
+		w_trj[:,k] .= w
     end
-	return part_pos, weights
+	return x_trj, w_trj
 end
 function assimilate()
     # Often much larger.
-	Np = 100
+	Np = 1000
 	K = 10
-	d = 3
 	x = rand(d,Np)
-	s = 0.001
-	x_true = ones(K)
-	x0_true = 2*pi*rand()
-	x_true[1] = x0_true
-	y = zeros(K)
+	s = 0.1
+	x_true = ones(d,K)
+	x0_true = 2*pi*rand(d)
+	Nrunup = 1000
+	for i = 1:Nrunup
+		x0_true = next(x0_true, 0.0)
+	end
+	for i = 1:Nrunup
+		for j = 1:Np
+			x[:,j] = next(x[:,j],s)
+		end
+	end
+	x_true[:,1] .= x0_true
+	y = zeros(d,K)
 	for i = 2:K
 	    x_true[:,i] = next(x_true[:,i-1], s)
-		y[i] = obs(x_true[:,i]) + σ_o*randn()
+		y[:,i] = obs(x_true[:,i]) + σ_o*randn(d)
 	end
 	N_thr = 20
 	# store trajectory of w and x.
