b04bdf33164963c1432b1d3c5bb0e2f7ac60ddde
Nisha Chandramoorthy <nishac@mit.edu>
2021-07-20 10:54:17 -0400

    Add new file for 1D

--- src/sir_1D.jl
+++ src/sir_1D.jl
@@ -1,28 +1,20 @@
-include("../examples/lorenz.jl")
+include("../examples/tent.jl")
 using JLD
 σ_o = 0.1
 function p_y_g_x(a)
 		return log(1/sqrt(2π)/σ_o) -0.5*a*a/σ_o/σ_o
 end
 function obs(x)
-	return x[3]
+    return x
 end
-function sir(y,x_ip,s,N_thr=10)
+function sir(y,x,s,N_thr=10)
 	K = size(y)[1]
-	N_p = size(x)[2]
-	d = size(x)[1]
-	weights = zeros(N_p, K)
-	x_trj = zeros(d, N_p, K)
-
-
+	N_p = size(x)[1]
     w = ones(N_p)./N_p
-	new_pts = similar(x_ip)
-	x = copy(x_ip)
-	
+	new_pts = similar(x)
 	for k = 1:K
     	for i = 1: N_p 
-				logwi = log(w[i]) + p_y_g_x(y[k] - obs(x[:,i]))
-				w[i] = exp(logwi)
+				log(w[i]) = log(w[i]) + p_y_g_x(y[k] - obs(x[i]))
     	end
 		@show sum(w)
 		w ./= sum(w)
@@ -36,7 +28,7 @@ function sir(y,x_ip,s,N_thr=10)
 			    r = rand()
 			    for i = 1:N_p
 		    	    if cdfw[i] >= r
-					    new_pts[:,j] .= x[:,i]
+					    new_pts[j] = x[i]
 			            break
 			        end
 		        end
@@ -44,30 +36,27 @@ function sir(y,x_ip,s,N_thr=10)
 			x .= new_pts
 	    end
 		x .= next.(x,s)
-		part_pos[:,:,k] .= x
-		weights[:,k] .= w
     end
-	return part_pos, weights
+	return x, w
 end
 function assimilate()
     # Often much larger.
 	Np = 100
 	K = 10
-	d = 3
-	x = rand(d,Np)
+	x = rand(Np)
 	s = 0.001
 	x_true = ones(K)
 	x0_true = 2*pi*rand()
 	x_true[1] = x0_true
 	y = zeros(K)
 	for i = 2:K
-	    x_true[:,i] = next(x_true[:,i-1], s)
-		y[i] = obs(x_true[:,i]) + σ_o*randn()
+	    x_true[i] = next(x_true[i-1], s)
+		y[i] = x_true[i] + σ_o*randn()
 	end
 	N_thr = 20
 	# store trajectory of w and x.
-	x_trj, w_trj = sir(y, x, s, N_thr) 
-	return x_trj, w_trj, x_true  
+	x, w = sir(y, x, s, N_thr) 
+	return x, w, x_true[K]     
 end
 
 
