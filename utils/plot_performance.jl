366d6944fedbb513c37123205616c6319e4f1d34
Nisha Chandramoorthy <nishac@mit.edu>
2021-08-02 15:18:06 -0400

    Add data and plotting utiliity

--- utils/plot_performance.jl
+++ utils/plot_performance.jl
@@ -1,7 +1,15 @@
 using PyPlot
 using JLD
 function plot_rmse_vs_Np()
-	
-
+    X = load("../data/mean_rmse_vs_Np.jld")
+    Np = X["Np"]
+	mean_rmse = X["mean_rmse"]
+	fig, ax = subplots()
+	ax.grid(true)
+	ax.plot(Np, mean_rmse, "bo--",lw=1.0,ms=20,label="RMSE") 
+	ax.legend(fontsize=28)
+	ax.set_xlabel("# of particles", fontsize=28)
+	ax.xaxis.set_tick_params(labelsize=28)
+	ax.yaxis.set_tick_params(labelsize=28)
 end
 
