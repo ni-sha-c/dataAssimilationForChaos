4e49431f9ab090f6311e0d8771aeb4f6d205ee0a
Nisha Chandramoorthy <nishac@mit.edu>
2022-01-25 18:30:10 -0500

    add select pts so as to mimic resampling

--- utils/plot_cat.jl
+++ utils/plot_cat.jl
@@ -4,7 +4,7 @@ using LinearAlgebra
 include("../examples/cat.jl")
 # y = v1.x1 + v2.x2 + epsilon
 rot_mat = reshape([q[1], -q[2], q[2], q[1]], 2, 2)
-function select_pts(v, ϵ, y, N)
+function put_pts(v, ϵ, y, N)
     npts = 0
 	pts = zeros(d,N)
 	clrs = zeros(N)
@@ -20,6 +20,17 @@ function select_pts(v, ϵ, y, N)
     end
 	return pts, clrs
 end
+function select_pts(v, ϵ, y, x, clrs)
+    npts = 0
+	N = size(clrs)
+	a = zeros(N)
+    for i = 1:N
+		a = dot(x[:,i],v)
+	end
+	sel = (a .> y - ϵ) && (a .< y + ϵ)
+    return x[sel], clrs[sel]
+end
+
 function plot_pts(x, xtrue, clrs, str=" ")
 	fig, ax = subplots()
     ax.grid(true)
@@ -48,19 +59,27 @@ function plot_sample_orbits(N, T, Δ)
 	v = [1.0, 0.0]
 	ϵ = 0.1
 	xtrue = rand(d)
-	x = rand(d, N)
+	r = -1.0 + 2*rand()
+	y = dot(v, xtrue) + ϵ*r
+	x, clrs = put_pts(v, ϵ, y, N)
+	plot_pts(x, xtrue, clrs, string("t = ", t*Δ, ", analysis"))
+
+
+    n = N
 	for t = 1:T
-        r = -1.0 + 2*rand()
-		y = dot(v, xtrue) + ϵ*r
-		x, clrs = select_pts(v, ϵ, y, N)
-		plot_pts(x, xtrue, clrs, string("t = ", t*Δ, ", analysis"))
 		for t1 = 1:Δ
 			xtrue = next(xtrue, 0)
-			for k = 1:N
+			n = size(x)[2]
+			for k = 1:n
 				x[:,k] = next(x[:,k],0)
 			end
 		end
         plot_pts(x, xtrue, clrs, string("t = ", t*Δ, ", forecast"))
+
+		r = -1.0 + 2*rand()
+		y = dot(v, xtrue) + ϵ*r
+		x, clrs = select_pts(v, ϵ, y, x, clrs)
+
 	end
 end
 
