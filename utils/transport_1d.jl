badf33144e240bede68651259d3dff651e4a7a17
Nisha Chandramoorthy <nishac@mit.edu>
2022-05-21 20:42:21 -0400

    add forecast

--- utils/transport_1d.jl
+++ utils/transport_1d.jl
@@ -9,9 +9,19 @@ function evolve_prob(s, N, T)
 	end
 	return x
 end
+function forecast(x, s)
+	for (i, xi) = enumerate(x)
+		x[i] = next(xi, s)
+	end
+	return x
+end
 function plot_srb(s)
 	x = evolve_prob(s, 100000, 40) 
 	hist(x, bins=100, density=true)
+end
+function cdf(x, xarr)
+	N = size(xarr)[1]
+	return 1/N*sum(xarr .<= x)
 end
 function generate_data(s,T,ϵ)
 	x = rand()
@@ -28,4 +38,39 @@ function generate_data(s,T,ϵ)
 		y_t[t] = x_t[t] + ϵ*randn()
 	end
 	return x_t, y_t
+end
+function sample_tn(N, m, v, a, b)
+	g = 1
+	k = 0
+	x = zeros(N)
+	while g <= N
+		pt = m + v*randn()
+		if a <= pt <= b
+			x[g] = pt
+			g = g + 1
+		end
+		k = k + 1
+	end
+	@show k
+	return x
+end
+function plot_dist(x, t, fig, ax)
+	ax.hist(x, density=true, histtype="step", 
+			label=string("Time = ", string(t)))
+end
+function analysis(x, s, y)
+	for (i, xi) = enumerate(x)
+		x[i] = next(xi, s)
+	end
+	return x
+end
+function analytical_filtering(s,T,ϵ,N)
+	x_true, y = generate_data(s,T,ϵ)
+	x = rand(N)
+	fig, ax = subplots()
+	for t = 1:T
+		x = forecast(x, s)	
+		plot_dist(x, string("ft ", t), fig, ax)
+		x = analysis(x, s, y[t])
+	end
 end
